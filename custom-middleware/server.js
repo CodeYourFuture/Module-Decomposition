fd91def649dd9f87be60c32215ca8eee413bfca3
eediallo <elhhabdouldiallo@gmail.com>
2025-04-08 22:27:47 +0100

    Add middleware to parse request body as JSON array

--- custom-middleware/server.js
+++ custom-middleware/server.js
@@ -10,3 +10,31 @@ const authenticate = (req, res, next) => {
   }
   next();
 };
+
+const passRequestPostBodyAsJSONArray = (req, res, next) => {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+
+  req.on("end", () => {
+    try {
+      const jsonBody = JSON.parse(body);
+      if (
+        Array.isArray(jsonBody) &&
+        jsonBody.every((item) => typeof item === "string")
+      ) {
+        req.body = jsonBody;
+        next();
+      } else {
+        res.status(400).send("Body must be an array of strings");
+      }
+    } catch (e) {
+      res.status(400).send("Invalid JSON format");
+    }
+  });
+
+  req.on("error", () => {
+    res.status(500).send("Error processing request");
+  });
+};
