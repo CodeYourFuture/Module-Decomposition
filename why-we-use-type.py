43fd3cc1baf89d371509537e797db9f20fb0104e
eediallo <elhhabdouldiallo@gmail.com>
2025-04-29 18:37:58 +0100

    predict what double("22") will do

---
+++ why-we-use-type.py
@@ -0,0 +1,25 @@
+def half(value):
+    return value / 2 
+
+def double(value):
+    return value * 2 
+
+def second(value):
+    return value[1]
+
+
+
+# print(half(22))
+# print(half("hello"))
+# print(half("22"))
+
+# print(double(22))
+# print(double("hello"))
+# print(double("22"))
+
+# print(second(22))
+# print(second(0x16))
+# print(second("hello"))
+# print(second("22"))
+
+print(double("22")) # will print 2222
