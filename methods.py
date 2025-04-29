eab6400c81028455e675a7faacdb09b1c4bfaeac
eediallo <elhhabdouldiallo@gmail.com>
2025-04-29 19:21:37 +0100

    change Person class to take a date of birth and update is_adult method

---
+++ methods.py
@@ -0,0 +1,15 @@
+from datetime import date
+
+class Person:
+    def __init__(self, name: str, DoB: date, preferred_operating_system: str):
+        self.name = name
+        self.DoB = DoB
+        self.preferred_operating_system = preferred_operating_system
+
+    def is_adult(self)-> bool:
+        current_date = date.today().year
+        age = current_date - self.DoB.year
+        return age >= 18
+
+imran = Person("Imran", date(1998, 1, 6), "Ubuntu")
+print(imran.is_adult())
