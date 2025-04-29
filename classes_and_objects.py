d534963e9f108fc40c589d591ed858788f6789e3
eediallo <elhhabdouldiallo@gmail.com>
2025-04-29 19:08:57 +0100

    add method to check if a person is an adult

--- classes_and_objects.py
+++ classes_and_objects.py
@@ -4,10 +4,14 @@ class Person:
         self.age = age
         self.preferred_operating_system = preferred_operating_system
 
+    def is_adult(self) -> bool:
+            return self.age >= 18
+
 imran = Person("Imran", 22, "Ubuntu")
 print(imran.name)
-print(imran.address) # address is not an attribute of the Person class
+# print(imran.address) # address is not an attribute of the Person class
+print(imran.is_adult())
 
 eliza = Person("Eliza", 34, "Arch Linux")
 print(eliza.name)
-print(eliza.address)# address is not an attribute of the Person class
+# print(eliza.address)# address is not an attribute of the Person class
