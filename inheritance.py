e9d929fe4c54962c0785c8903399a64de9ca080a
eediallo <elhhabdouldiallo@gmail.com>
2025-04-30 09:19:35 +0100

    Refine type annotations for previous_last_names and change_last_name method in Child class

--- inheritance.py
+++ inheritance.py
@@ -10,9 +10,9 @@ class Parent:
 class Child(Parent):
     def __init__(self, first_name: str, last_name: str):
         super().__init__(first_name, last_name)
-        self.previous_last_names = []
+        self.previous_last_names: list[str] = []
 
-    def change_last_name(self, last_name) -> None:
+    def change_last_name(self, last_name:str) -> None:
         self.previous_last_names.append(self.last_name)
         self.last_name = last_name
 
