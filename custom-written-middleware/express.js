26e0c798e34ea0d7d8447ac8e40fb8a9eee0614a
Droid-An <andrei.filippov.aaf@gmail.com>
2025-09-12 17:17:42 +0100

    done custom written middleware

---
+++ custom-written-middleware/express.js
@@ -0,0 +1,64 @@
+import express from "express";
+const app = express();
+
+const assignHeader = (req, res, next) => {
+  req.username = req.headers["x-username"] ? req.headers["x-username"] : null;
+  next();
+};
+
+const parseJSON = (req, res, next) => {
+  const bodyBytes = [];
+  req.on("data", (chunk) => bodyBytes.push(...chunk));
+  req.on("end", () => {
+    const bodyString = String.fromCharCode(...bodyBytes);
+    let body;
+    try {
+      body = JSON.parse(bodyString);
+    } catch (error) {
+      console.error(`Failed to parse body ${bodyString} as JSON: ${error}`);
+      res.status(400).send("Expected body to be JSON.");
+      return;
+    }
+    if (
+      typeof body != "object" ||
+      body.some((element) => typeof element !== "string")
+    ) {
+      console.error(
+        `Failed to extract text of the message from post body: ${bodyString}`
+      );
+      res
+        .status(400)
+        .send("Expected body to be a JSON object containing key message.");
+      return;
+    }
+    req.body = body;
+
+    next();
+  });
+};
+
+app.use(assignHeader);
+app.use(parseJSON);
+
+app.post("/", (req, res) => {
+  let message = [];
+  if (req.username) {
+    message.push(`You are authenticated as ${req.username}`);
+  } else {
+    message.push("You are not authenticated.");
+  }
+  if (req.body.length > 0) {
+    message.push(
+      `You have requested information about ${
+        req.body.length
+      } subjects: ${req.body.join(", ")}\n`
+    );
+  } else {
+    message.push("You have requested information about 0 subjects.\n");
+  }
+  res.send(message.join("\n\n"));
+});
+
+app.listen(3000, () => {
+  console.error(`server listening on port 3000`);
+});
