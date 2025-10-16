037482f2e14387a2a11dd64932d885ee0abdf170
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-10-16 19:56:41 +0100

    using generic, frozen with data class

--- prep_exercises/exercise6.py
+++ prep_exercises/exercise6.py
@@ -4,16 +4,19 @@ from typing import List
 @dataclass(frozen=True)
 class Person:
     name: str
+    age: int
     children: List["Person"]
 
-fatma = Person(name="Fatma", children=[])
-aisha = Person(name="Aisha", children=[])
+fatma = Person(name="Fatma", age=8, children=[])
+aisha = Person(name="Aisha", age=6, children=[])
 
-imran = Person(name="Imran", children=[fatma, aisha])
+imran = Person(name="Imran", age=35, children=[fatma, aisha])
+
 
 def print_family_tree(person: Person) -> None:
     print(person.name)
     for child in person.children:
         print(f"- {child.name} ({child.age})")
+
 
 print_family_tree(imran)
