787d00c3f1e9c86604a26fd4e4841f789150281c
Rihanna P <114884466+RihannaP@users.noreply.github.com>
2025-10-25 20:31:21 +0100

    Implement usernameExtract for extracting username

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 import express from "express"
+const app = express();
+const port = 3000;
+
+function usernameExtract(req, res, next) {
+  const username = req.get("X-Username");
+  req.username = username ? username : null;
+  next();
+}
+
