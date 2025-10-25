8575a8a6f1651264966fd6f1bdd77850110032b2
Rihanna P <114884466+RihannaP@users.noreply.github.com>
2025-10-25 21:02:17 +0100

    Implement post and check Array is json Arr

--- script.js
+++ script.js
@@ -2,9 +2,21 @@ import express from "express"
 const app = express();
 const port = 3000;
 
-function usernameExtract(req, res, next) {
+function usernameExtractor(req, res, next) {
   const username = req.get("X-Username");
   req.username = username ? username : null;
   next();
 }
 
+app.post("/", usernameExtractor,(req, res) => {
+  const username = req.username;
+  const subjects = req.body;
+
+    if (!Array.isArray(subjects)) {
+        return res.status(400).send("Error: Body must be a JSON array.");
+    }
+
+
+
+  res.send(response);
+});
