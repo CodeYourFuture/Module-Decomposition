d542bc4062699ba333c49bb6a71f337a8fdc8816
Fatma <fatma.arslantas@outlook.com>
2025-11-09 21:23:33 +0000

    feat: calculate age automatically based on birth year

--- generics.py
+++ generics.py
@@ -1,18 +1,24 @@
 from dataclasses import dataclass
 from typing import List
+from datetime import date
 
 @dataclass(frozen=True)
 class Person:
     name: str
-    age: int
+    birth_year: int
     children: List["Person"]
 
-fatma = Person(name="Fatma", age=7, children=[])
-aisha = Person(name="Aisha", age=5, children=[])
-imran = Person(name="Imran", age=35, children=[fatma, aisha])
+    @property
+    def age(self) -> int:
+        current_year = date.today().year
+        return current_year - self.birth_year
+
+fatma = Person(name="Fatma", birth_year=2018, children=[])
+aisha = Person(name="Aisha", birth_year=2020, children=[])
+imran = Person(name="Imran", birth_year=1990, children=[fatma, aisha])
 
 def print_family_tree(person: Person) -> None:
-    print(person.name)
+    print(f"{person.name} ({person.age})")
     for child in person.children:
         print(f"- {child.name} ({child.age})")
 
