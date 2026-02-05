b3a0fde480c6a635283cc443394ba1b5f8d16098
mikiyas-stp <mikiyasgebremichaell@gmail.com>
2026-02-05 22:40:45 +0000

    i have used a birth year instead of a fixed age using a python lib datetime to have age instead of hardcoding it

--- prep_exercises/exercise6.py
+++ prep_exercises/exercise6.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import List
+from datetime import date
 
 @dataclass(frozen=True)
 class Person:
@@ -7,16 +8,18 @@ class Person:
     age: int
     children: List["Person"]
 
-fatma = Person(name="Fatma", age=8, children=[])
-aisha = Person(name="Aisha", age=6, children=[])
+fatma = Person(name="Fatma", birth_year=2011, children=[])
+aisha = Person(name="Aisha", birth_year=2015, children=[])
 
-imran = Person(name="Imran", age=35, children=[fatma, aisha])
+imran = Person(name="Imran", birth_year=2000, children=[fatma, aisha])
 
+def current_age(birth_year:int) -> int:
+    today = date.today()
+    return today.year - birth_year
 
 def print_family_tree(person: Person) -> None:
-    print(person.name)
+    print(f"{person.name} ({current_age(person.birth_year)})")
     for child in person.children:
-        print(f"- {child.name} ({child.age})")
-
+        print(f"- {child.name} ({current_age(child.birth_year)})")
 
 print_family_tree(imran)
