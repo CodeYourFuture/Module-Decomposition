8dc2dcc6e5338fa2cb94121c4f67f5c4815487f8
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-02-13 00:45:53 +0000

    resuing shared codebase from custom middleware version, remove part are no longer relevant before adding off-the-shelf body parsing middleware

---
+++ off-the-shelf-middleware/index.js
@@ -0,0 +1,31 @@
+// Middleware 1 looks for a header with name "X-Username"
+function usernameMiddleware(request, response, next) {
+  const usernameHeader = request.header("X-Username");
+  request.username = usernameHeader ? usernameHeader : null;
+  next();
+}
+
+
+
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.use(usernameMiddleware);
+
+
+app.post("/", (request, response) => {
+  let responseToClient;
+  if (request.username) {
+    responseToClient = `You are authenticated as ${request.username}.`;
+  } else {
+    responseToClient = "You are not authenticated.";
+  }
+
+  response.send(`${responseToClient}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
