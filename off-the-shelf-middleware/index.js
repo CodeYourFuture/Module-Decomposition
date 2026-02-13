f6b9b6e9d16a86d3e76212d37871d63d3513027d
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-02-13 00:49:40 +0000

    Add the built-in JSON parser middleware

--- off-the-shelf-middleware/index.js
+++ off-the-shelf-middleware/index.js
@@ -1,11 +1,9 @@
-// Middleware 1 looks for a header with name "X-Username"
+// Middleware 1 - custom middleware - looks for a header with name "X-Username"
 function usernameMiddleware(request, response, next) {
   const usernameHeader = request.header("X-Username");
   request.username = usernameHeader ? usernameHeader : null;
   next();
 }
-
-
 
 const express = require("express");
 
@@ -13,6 +11,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(usernameMiddleware);
+app.use(express.json());
 
 
 app.post("/", (request, response) => {
