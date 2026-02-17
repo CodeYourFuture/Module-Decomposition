7edf1bc63817664bb1d53cc93f82d2cb7758fe15
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-02-17 23:29:57 +0000

    Feat:Adding second half to the response base on the array.

--- custom-middlewares/app.js
+++ custom-middlewares/app.js
@@ -3,15 +3,22 @@ const extractUsername = require("./middleware/extractUsername");
 const parseJsonArrayBody = require("./middleware/parseJsonArrayBody");
 const app = express();
 
-app.post("/subjects",extractUsername,parseJsonArrayBody,
-  (req, res) => {
+app.post("/subjects", extractUsername, parseJsonArrayBody, (req, res) => {
+  const auth = req.username
+    ? `You are authenticated as ${req.username}.`
+    : "You are not authenticated.";
 
-    const auth = req.username
-      ? `You are authenticated as ${req.username}.`
-      : `You are not authenticated.`;
+  const subjectCount = req.body.length;
+  const subjectLabel = subjectCount === 1 
+       ? "subject" 
+       : "subjects";
+  const subjectList = req.body.join(", ");
+  const subjectMessage =
+    subjectCount === 0
+      ? "You have requested information about 0 subjects."
+      : `You have requested information about ${subjectCount} ${subjectLabel}: ${subjectList}.`;
 
-    res.send(auth);
-  }
-);
+  res.send(`${auth} ${subjectMessage}`);
+});
 
 module.exports = app;
